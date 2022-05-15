2eefc1ae164b6026cbfe4354d8c8646c43791de3
David <davidkramm53@gmail.com>
2022-05-14 23:47:51 -0500

    initial html and JS

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Finds the search button
+const btnSearch = document.getElementById('search-submit');
+
+const input = document.getElementById('city')
+
+let prevSearch = [];
+
+const select = document.getElementById('cities')
+
+const apiKey = '67febe86d8dd4147336015e28a109e2e';
